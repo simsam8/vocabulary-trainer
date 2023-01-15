b1b41c48b6803e9a253250ffbc96dcfee2e546fc
simsam8 <simvedaa@gmail.com>
2023-01-15 18:36:28 +0100

    interface and choose language methods

--- vocab.py
+++ vocab.py
@@ -8,13 +8,20 @@ class Vocab:
         with open("vocabs.json", "r") as f:
             self.languages = json.load(f)
 
-    def interface(self):
-        print("\n- - - - - - - - - - - -")
-        print("1.\tAdd Word Pair")
-        print("2.\tAdd Language")
-        print("3.\tSave")
-        print("4.\tQuit")
-        print("- - - - - - - - - - - -\n")
+    def interface(self, interface_type):
+        if interface_type == "main":
+            print("\n- - - - - - - - - - - -")
+            print("1.\tAdd Word Pair")
+            print("2.\tAdd Language")
+            print("3.\tSave")
+            print("4.\tQuit")
+            print("- - - - - - - - - - - -\n")
+
+        elif interface_type == "language":
+            print("\n- - - - - - - - - - - -")
+            for i, language in enumerate(self.languages):
+                print(f"{i}.\t{language}")
+            print("\n- - - - - - - - - - - -")
 
     def add_language(self):
         new_lang = input("Language to add: ").lower()
@@ -25,14 +32,20 @@ class Vocab:
             print(f"{new_lang} is already in languages")
         # print(self.languages)
 
+    def choose_language(self):
+        self.interface("language")
+        lang_id = int(input("Choose language: ").lower())
+        language = list(self.languages.items())[lang_id][0]
+        return language
+
     def add_pair(self):
-        language = input("Choose language: ").lower()
+        language = self.choose_language()
         unknown = input(f"New word in {language}: ").lower()
         known = input("Word in English: ").lower()
         if len(self.languages[language]) == 0:
             pair_id = 0
         else:
-            pair_id = len(self.languages[language]) - 1
+            pair_id = len(self.languages[language])
 
         pair_data = {
             "id": pair_id,
@@ -56,7 +69,7 @@ def main():
     voc = Vocab()
     voc.show_data()
     while True:
-        voc.interface()
+        voc.interface("main")
         choice = int(input("Choose option: "))
         if choice == 1:  # Add word pair
             voc.add_pair()
