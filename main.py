be75a34c9e61066769eb4268b8833f9d2d7c1890
simsam8 <simvedaa@gmail.com>
2023-01-17 19:40:05 +0100

    change current file function

--- main.py
+++ main.py
@@ -17,6 +17,14 @@ def interface():
     print("- - - - - - - - - - - -\n")
 
 
+def change_file():
+    global FILE_TO_USE, game_tester, vocab
+    new_file = input("Input file to change to: ")
+    FILE_TO_USE = new_file
+    game_tester = Tester(FILE_TO_USE)
+    vocab = Vocab(FILE_TO_USE)
+
+
 def main():
     while True:
         interface()
@@ -29,7 +37,7 @@ def main():
         if choice == 3:
             vocab.access_vocab()
         if choice == 4:
-            return
+            change_file()
         if choice == 5:
             print("Saving and Exiting program")
             vocab.save()
