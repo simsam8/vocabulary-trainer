950809fab4970917fe293770bca49bf6d69608c2
simsam8 <simvedaa@gmail.com>
2023-01-17 22:41:08 +0100

    able to guess a random pair of words

--- vocab_tester.py
+++ vocab_tester.py
@@ -21,7 +21,19 @@ class Vocab_tester:
     def random_pair(self):
         random_index = randrange(0, len(self.vocabulary[self.language]))
         pair = self.vocabulary[self.language][random_index]
-        print(pair)
+        fifty_fifty = randrange(0, 2)
+        if fifty_fifty == 0:
+            display_word = pair["unknown"]
+            guess_word = pair["known"]
+        elif fifty_fifty == 1:
+            display_word = pair["known"]
+            guess_word = pair["unknown"]
+
+        user_guess = input(f"Write the translation of {display_word}: ")
+        if user_guess == guess_word:
+            print("Correct!")
+        else:
+            print("Wrong!")
 
 
 # test = Vocab_tester("vocabs.json")
